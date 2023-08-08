8cc8b9bf333ae9f57c53532d0e803f30ddd56ab8
Jishnu <jishnu@CT129-JISHNURAJ-K.local>
2023-08-08 15:47:52 +0530

    crcted

--- Assignment1/javascript/script.js
+++ Assignment1/javascript/script.js
@@ -1,4 +1,7 @@
 /* -------------    QUESTION 1 ---------------- */
+//Add the complete qn as a comment
+// Use camel case for variable names.
+// Check the variable declarations in JavaScript and select the one that is appropriate for your variables.
 
 //Premitive Data types
 
@@ -7,6 +10,8 @@ let name = "Anagha";
 console.log("The data type of the variable 'name' is: " + typeof name);
 
 //Number data type
+//Why did you used let for decalaration?
+
 let num = 15;
 console.log("The data type of the variable 'num' is: " + typeof num);
 
@@ -16,44 +21,59 @@ console.log("The data type of the variable 'bignum' is: " + typeof bignum);
 
 //Boolean data type
 let completed = true;
-console.log("The data type of the variable 'completed' is: " + typeof completed);
+console.log(
+  "The data type of the variable 'completed' is: " + typeof completed
+);
 
 //Undefined data type
 let content;
 console.log("The data type of the variable 'content' is: " + typeof content);
 
 //Null data type
-let nothing = null
+let nothing = null;
 console.log("The data type of the variable 'nothing' is: " + typeof nothing);
 
 //Symbol data type
-let val = Symbol('welcome');
+let val = Symbol("welcome");
 console.log("The data type of the variable 'val' is: " + typeof val);
 
 //Non-Premitive Data types
 //Object data type
 
 //Array
-let fruits = ["apple", "orange", "mango"]
+let fruits = ["apple", "orange", "mango"];
 console.log("The data type of the variable 'fruits' is: " + typeof fruits);
 
+//check whether function is a data type or not
 //Function
-function greeting(){
-    console.log("Good Morning!");
+function greeting() {
+  console.log("Good Morning!");
 }
 console.log("The data type of the variable 'greeting' is: " + typeof greeting);
 
 //Object
-let obj = {a:123, b:456,c:789};
+let obj = { a: 123, b: 456, c: 789 };
 console.log("The data type of the variable 'obj' is: " + typeof obj);
 
 /* -------------    QUESTION 2 ---------------- */
 
-let strTemplateLiterals =(`${name} ${num} ${bignum} ${completed} ${content} ${nothing} ${fruits}`);
-console.log(strTemplateLiterals); 
+let strTemplateLiterals = `${name} ${num} ${bignum} ${completed} ${content} ${nothing} ${fruits}`;
+console.log(strTemplateLiterals);
 
-
-let strWithoutTemplateLiterals = (name + " " + num + " " +bignum + " " +completed + " " + content + " " + nothing + " " + fruits);
+let strWithoutTemplateLiterals =
+  name +
+  " " +
+  num +
+  " " +
+  bignum +
+  " " +
+  completed +
+  " " +
+  content +
+  " " +
+  nothing +
+  " " +
+  fruits;
 console.log(strWithoutTemplateLiterals);
 
 /* -------------    QUESTION 3 ---------------- */
--- Assignment2/javascript/script.js
+++ Assignment2/javascript/script.js
@@ -1,48 +1,52 @@
+//Give proper names
 let arr = ["1", "2", "3", "4", "5", "6", "7"];
 
 //1. a. Remove number "6" from the array and console the length of the array
 
+// Don't hardcode the position of 6. find it.
 arr.splice(5, 1);
-len = arr.length
+len = arr.length;
 console.log("The length of the array is : " + len);
 
 /* b. Convert all the items of the array to data type number and console each items data type, 
 use any of the array iteration methods provided by JS for iteration. */
 
+//Read the question properly.
 arr.forEach((n) => {
-    elementDatatypeNumber = Number(n);
-    console.log("Data type of " + n, "is : " + typeof elementDatatypeNumber);
-})
+  //where is declaration
+  elementDatatypeNumber = Number(n);
+  console.log("Data type of " + n, "is : " + typeof elementDatatypeNumber);
+});
 
 /* c. Remove last three items of the array, use JS provided array method, 
 then console the array and then add "one" and "two" (strings) to the beginning of the array and console the array. */
 
 arr.splice(-3, 3);
 console.log(arr);
+//check other methods
 arr.splice(0, 0, "One", "Two");
 console.log(arr);
 
 /* d. Using any one of the array iteration methods console the string concatenation of all items 
 of the array and also console the sum of all the items ( convert to integer before calculating)*/
-
+//what will be the value of arr while reaching this line
+//do both task with a single loop
 let concatenatedString = "";
 arr.forEach((n) => {
-    concatenatedString = concatenatedString.concat(n)
-})
+  concatenatedString = concatenatedString.concat(n);
+});
 console.log(concatenatedString);
-
 
 arr = ["1", "2", "3", "4", "5", "6", "7"];
 sum = 0;
 arr.forEach((n) => {
-    let elementOfDatatypeInteger = parseInt(n);
-    sum = sum + elementOfDatatypeInteger
-})
+  let elementOfDatatypeInteger = parseInt(n);
+  sum = sum + elementOfDatatypeInteger;
+});
 console.log(sum);
 
-
 /* e. Filter out item "3" from the array and console the array (use any array method)*/
-
+//read the question properly
 let indexValue = arr.indexOf("3");
 arr.splice(indexValue, 1);
 console.log(arr);
@@ -52,35 +56,35 @@ console.log(arr);
 arr = ["1", "2", "3", "4", "5", "6", "7"];
 
 arr.forEach((n) => {
-    if ((n == "3") || (n == "6") || (n == "7")) {
-        console.log(n);
-    }
-})
+  //use ===
+  if (n == "3" || n == "6" || n == "7") {
+    console.log(n);
+  }
+});
 
 /* g. [1, 2, "3", 4, 5, 6, "7"]  Compare this array with the above given array and console only if both items 
 of the array have same data type. (Compare each item of this array with each item of the other array) */
-
-let arr2 = [1, 2, "3", 4, 5, 6,"7"];
+//write the output and describe it's working
+let arr2 = [1, 2, "3", 4, 5, 6, "7"];
 
 arr.forEach((num1, index) => {
-    let num2 = arr2[index];
-    if(typeof arr[index] == typeof arr2[index]){
-        console.log(+num1,+num2);
-    }
-     
-  });
+  let num2 = arr2[index];
+  if (typeof arr[index] == typeof arr2[index]) {
+    console.log(+num1, +num2);
+  }
+});
 
 /* h. [0,2,3,7,5,6,8] iterate the array and multiply each item by its 
 index value and console the result only if result is greater than 40. */
 
-let arr3 = [0, 2 , 3, 7, 5, 6, 8];
+let arr3 = [0, 2, 3, 7, 5, 6, 8];
 
 arr3.forEach((i) => {
-    let multipliedValue = arr3[i] * i;
-    if (multipliedValue > 40) {
-        console.log(multipliedValue);
-      }
-})
+  let multipliedValue = arr3[i] * i;
+  if (multipliedValue > 40) {
+    console.log(multipliedValue);
+  }
+});
 
 // i. Create two arrays with five items each and merge the array into a single array and then console it.
 let firstArray = [1, 2, 3];
