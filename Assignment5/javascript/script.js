2e9456163ad79aecb2896d1d3e08ba808a155155
Jishnu <jishnu@CT129-JISHNURAJ-K.local>
2023-08-18 15:36:47 +0530

    crction

--- Assignment5/javascript/script.js
+++ Assignment5/javascript/script.js
@@ -12,84 +12,104 @@ if so, then show an error msg stating only numbers are allowed. */
 /*c. The minimum length of the phone number should be 10,
 otherwise, show the corresponding error msg below the mobile no. field */
 
-const name = document.getElementById('name');
-const phone = document.getElementById('phone');
-const place = document.getElementById('place');
-const company = document.getElementById('company');
-const pincode = document.getElementById('pincode');
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+const place = document.getElementById("place");
+const company = document.getElementById("company");
+const pincode = document.getElementById("pincode");
 
 function formValidate() {
-  if (name.value === '' || name.value == null) {
-    nameErrorContainer.innerText = 'This field is required.';
+  if (name.value === "" || name.value == null) {
+    nameErrorContainer.innerText = "This field is required.";
   }
-  if (phone.value === '' || phone.value == null) {
-    phoneErrorContainer.innerText = 'This field is required.';
+  if (phone.value === "" || phone.value == null) {
+    phoneErrorContainer.innerText = "This field is required.";
   } else if (!/^\d+$/.test(phone.value)) {
-    phoneErrorContainer.innerText = 'Only numbers are allowed';
-  } else if ((phone.value).length < 10) {
-    phoneErrorContainer.innerText = 'Length of the phone number should be 10';
+    phoneErrorContainer.innerText = "Only numbers are allowed";
+  } else if (phone.value.length < 10) {
+    phoneErrorContainer.innerText = "Length of the phone number should be 10";
   }
-  if (place.value === '' || place.value == null) {
-    placeErrorContainer.innerText = 'This field is required.';
+  if (place.value === "" || place.value == null) {
+    placeErrorContainer.innerText = "This field is required.";
   }
-  if (company.value === '' || company.value == null) {
-    companyErrorContainer.innerText = 'This field is required.';
+  if (company.value === "" || company.value == null) {
+    companyErrorContainer.innerText = "This field is required.";
   }
-  if (pincode.value === '' || pincode.value == null) {
-    pincodeErrorContainer.innerText = 'This field is required.';
+  if (pincode.value === "" || pincode.value == null) {
+    pincodeErrorContainer.innerText = "This field is required.";
   } else if (!/^\d+$/.test(pincode.value)) {
-    pincodeErrorContainer.innerText = 'Only numbers are allowed';
+    pincodeErrorContainer.innerText = "Only numbers are allowed";
   }
 }
 
-formSubmitBtn.addEventListener('click', formValidate);
+// Submit the values only when all the validations are successful.
+// If any field has a validation issue, there is no need to clear the values of all the other fields.
+formSubmitBtn.addEventListener("click", formValidate);
 
 /*d. On submission of the form, store the details in the local storage and clear the form.
 (it should stay on the same page don't refresh the page). */
 
 function storeDetails() {
-  localStorage.setItem('Name', name.value);
-  localStorage.setItem('Phone', phone.value);
-  localStorage.setItem('Place', place.value);
-  localStorage.setItem('Company', company.value);
-  localStorage.setItem('Pincode', pincode.value);
+  //store it as single object
+  localStorage.setItem("Name", name.value);
+  localStorage.setItem("Phone", phone.value);
+  localStorage.setItem("Place", place.value);
+  localStorage.setItem("Company", company.value);
+  localStorage.setItem("Pincode", pincode.value);
 }
 
-formSubmitBtn.addEventListener('click', storeDetails);
+formSubmitBtn.addEventListener("click", storeDetails);
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll("input");
 function clearInputs() {
   inputs.forEach((input) => {
-    input.value = '';
+    input.value = "";
   });
 }
 
-formSubmitBtn.addEventListener('click', clearInputs);
+formSubmitBtn.addEventListener("click", clearInputs);
 
 /*e. Make a prepopulate button, which when clicked will populate the
  form with values in the local storage if it exists, otherwise, the button will be disabled. */
 
 function prepopulateDetails() {
-  document.getElementById('name').value = localStorage.getItem('Name', name.value);
-  document.getElementById('phone').value = localStorage.getItem('Phone', phone.value);
-  document.getElementById('place').value = localStorage.getItem('Place', place.value);
-  document.getElementById('company').value = localStorage.getItem('Company', company.value);
-  document.getElementById('pincode').value = localStorage.getItem('Pincode', pincode.value);
+  // get getElementById() on top and use the names here
+  document.getElementById("name").value = localStorage.getItem(
+    "Name",
+    name.value
+  );
+  document.getElementById("phone").value = localStorage.getItem(
+    "Phone",
+    phone.value
+  );
+  document.getElementById("place").value = localStorage.getItem(
+    "Place",
+    place.value
+  );
+  document.getElementById("company").value = localStorage.getItem(
+    "Company",
+    company.value
+  );
+  document.getElementById("pincode").value = localStorage.getItem(
+    "Pincode",
+    pincode.value
+  );
 }
 
-prepopulateBtn.addEventListener('click', prepopulateDetails);
+prepopulateBtn.addEventListener("click", prepopulateDetails);
 
 /*2. Create a button and div using JS, when clicked on the button your basic details
 should be shown in the div. The static HTML file should only contain the basic HTML structure,
 no div/buttons. */
 
-const displayBasicDetailsBtn = document.createElement('button');
-displayBasicDetailsBtn.innerText = 'Display';
+const displayBasicDetailsBtn = document.createElement("button");
+displayBasicDetailsBtn.innerText = "Display";
 document.body.appendChild(displayBasicDetailsBtn);
 
 function displayBasicDetails() {
+  //if localstorage having any other site data, how to handle that??
   for (let i = 0; i < localStorage.length; i += 1) {
-    const displayBasicDetailsContainer = document.createElement('div');
+    const displayBasicDetailsContainer = document.createElement("div");
     document.body.appendChild(displayBasicDetailsContainer);
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
@@ -97,7 +117,7 @@ function displayBasicDetails() {
     displayBasicDetailsContainer.appendChild(node);
   }
 }
-displayBasicDetailsBtn.addEventListener('click', displayBasicDetails);
+displayBasicDetailsBtn.addEventListener("click", displayBasicDetails);
 
 //3. Create a private variable and console it by accessing it.
 
@@ -111,29 +131,31 @@ const Student = ({ studentName, studentAge, studentPlace }) => {
     getAge: () => _age,
     getPlace: () => _place,
 
-    setPlace: (newPlace) => _place = newPlace,
+    setPlace: (newPlace) => (_place = newPlace),
   };
 };
 
-const student1 = Student({ studentName: 'Amil', studentAge: '13', studentPlace: 'India' });
+const student1 = Student({
+  studentName: "Amil",
+  studentAge: "13",
+  studentPlace: "India",
+});
 console.log(student1.getName());
 console.log(student1.getAge());
 console.log(student1.getPlace());
-student1.setPlace('Canada');
+student1.setPlace("Canada");
 console.log(student1.getPlace());
-
 
 //4. Create a static variable and console it by accessing it.
 
-class Employee{
-
+class Employee {
   static numberOfEmployees = 0;
 
-  static showMessage(){
+  static showMessage() {
     console.log("Welcome Onboard!");
   }
 
-  constructor(employeeName){
+  constructor(employeeName) {
     this.employeeName = employeeName;
     Employee.numberOfEmployees += 1;
   }
